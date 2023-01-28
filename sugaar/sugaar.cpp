cc6269bdde6eb4366744e222c7b41e62a45e2727
fpetm <fpetm@proton.me>
2023-01-29 00:11:26 +0100

    Add stb library as submodule

--- sugaar/sugaar.cpp
+++ sugaar/sugaar.cpp
@@ -1,6 +1,9 @@
 ﻿#include "sugaar.hpp"
 #include "math.hpp"
 
+#define STB_IMAGE_IMPLMENTATION
+#include "../stb/stb_image.h"
+
 int main() {
 	sugaar::Vector<3> vec(1, 2, 3);
 	std::cout << vec.X() << "\n";
